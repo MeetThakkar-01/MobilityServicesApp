989c5ba75dfae2501cf76ab98efcb732fbdb2ced
Meet Thakkar <meetthakkar05@gmail.com>
2020-03-02 00:40:50 +0530

    Initial Set Ups

---
+++ mobility_services_app/lib/services/auth.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // Sign in Anonymously
+
+  // Sign in with email & pass
+
+  //register with email and PAss
+
+  //sign out
+}
