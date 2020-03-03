5ba9db12830433c1c0026e4c37017666db1aefde
Mihir Poliwal <mihirpoliwal@gmail.com>
2020-03-03 19:52:55 +0530

    testing

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -4,7 +4,10 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("Home"),
+      child: Text(
+        "Home",
+        style: TextStyle(fontSize: 100),
+      ),
     );
   }
 }
